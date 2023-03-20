9769a1cbb66405af6f47d46818fd48ba5260f5c2
Sergey Getmanskiy <getmansky1997@yandex.ru>
2023-03-20 21:01:41 +0300

    bugfix after first review

--- script/script.js
+++ script/script.js
@@ -1,9 +1,15 @@
 let profile=document.querySelector('.profile');
 let editButton=profile.querySelector('.profile__edit-button');
+let profileUserName = profile.querySelector('.profile__user-name');
+let profileUserOccupation = profile.querySelector('.profile__user-occupation');
 let popup=document.querySelector('.popup');
-let closeButton=popup.querySelector('.popup__close-btn')
+let closeButton=popup.querySelector('.popup__close-btn');
+let formElement = document.querySelector('.input');
+let nameInput = formElement.querySelector('.input__text_type_user-name');
+let occupationInput =  formElement.querySelector('.input__text_type_user-occupation');
 
-function popUp() {
+
+function showPopUp() {
   popup.classList.add('popup_opened');
 }
 
@@ -11,23 +17,18 @@ function closePopUp () {
   popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', popUp);
+editButton.addEventListener('click', showPopUp);
 closeButton.addEventListener('click', closePopUp);
 
 
 // Обработчик «отправки» формы
-let formElement = document.querySelector('.input');
-let nameInput = formElement.querySelector('.input__text_type_username');
-let occupationInput =  formElement.querySelector('.input__text_type_useroccupation');
-
 function handleFormSubmit (evt) {
     evt.preventDefault();
     let userName = nameInput.value;
     let userOccupation = occupationInput.value;
-    let profileUserName = profile.querySelector('.profile__username');
-    let profileUserOccupation = profile.querySelector('.profile__useroccupation');
     profileUserName.textContent = userName;
     profileUserOccupation.textContent = userOccupation;
+    closePopUp();
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
