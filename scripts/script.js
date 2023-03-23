275a010f068516f9d6c397571bd30811f147bcef
Sergey Getmanskiy <getmansky1997@yandex.ru>
2023-03-23 20:20:33 +0300

    bugfix after second review

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,44 @@
 let profile=document.querySelector('.profile');
-let editButton=profile.querySelector('.profile__edit-button');
+let editButton=document.querySelector('.button_place_edit-button');
 let profileUserName = profile.querySelector('.profile__user-name');
 let profileUserOccupation = profile.querySelector('.profile__user-occupation');
 let popup=document.querySelector('.popup');
-let closeButton=popup.querySelector('.button-image_place_close-popup');
-let formElement = document.querySelector('.input');
-let nameInput = formElement.querySelector('.input__text_type_user-name');
-let occupationInput =  formElement.querySelector('.input__text_type_user-occupation');
+let closeButton=popup.querySelector('.button_place_close-popup');
+let formElement = document.querySelector('.form');
+let nameInput = formElement.querySelector('.form__input_type_user-name');
+let occupationInput =  formElement.querySelector('.form__input_type_user-occupation');
 
 
 function showPopUp() {
   popup.classList.add('popup_opened');
-  nameInput.setAttribute('value', profileUserName.textContent);
-  occupationInput.setAttribute('value', profileUserOccupation.textContent);
 }
 
-
-function closePopUp () {
+function closePopUp() {
   popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', showPopUp);
-closeButton.addEventListener('click', closePopUp);
+// Обработчик значения поля 'user-name'
+function handleUserName() {
+  nameInput.setAttribute('value', profileUserName.textContent);
+}
 
+// Обработчик значения поля 'user-occupation'
+function handleUserOccupation() {
+  occupationInput.setAttribute('value', profileUserOccupation.textContent);
+}
 
 // Обработчик «отправки» формы
 function handleFormSubmit (evt) {
-    evt.preventDefault();
-    let userName = nameInput.value;
-    let userOccupation = occupationInput.value;
-    profileUserName.textContent = userName;
-    profileUserOccupation.textContent = userOccupation;
-    closePopUp();
+  evt.preventDefault();
+  let userName = nameInput.value;
+  let userOccupation = occupationInput.value;
+  profileUserName.textContent = userName;
+  profileUserOccupation.textContent = userOccupation;
+  closePopUp();
 }
 
+editButton.addEventListener('click', showPopUp);
+editButton.addEventListener('click', handleUserName);
+editButton.addEventListener('click', handleUserOccupation);
+closeButton.addEventListener('click', closePopUp);
 formElement.addEventListener('submit', handleFormSubmit);
