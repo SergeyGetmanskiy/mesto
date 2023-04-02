f2e2f51b4954a20a7c83730508505f425b5ecbe4
Sergey Getmanskiy <getmansky1997@yandex.ru>
2023-04-02 19:01:34 +0300

    исправления после 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,20 @@
 const profile = document.querySelector('.profile');
-const editButton = profile.querySelector('.button_type_edit-button');
-const addButton = profile.querySelector('.button_type_add-button');
+const userName = profile.querySelector('.profile__user-name');
+const userOccupation = profile.querySelector('.profile__user-occupation');
+const buttonOpenEditProfilePopup = profile.querySelector('.button_type_edit-button');
+const buttonOpenAddCardPopup = profile.querySelector('.button_type_add-button');
 
 const popupEditProfile = document.querySelector('.popup_type_edit-profile');
-const closeButtonEditProfile = popupEditProfile.querySelector('.button_place_edit-profile');
+const closeButtonEditProfilePopup = popupEditProfile.querySelector('.button_place_edit-profile');
 const formEditProfile = popupEditProfile.querySelector('.form_type_edit-profile');
 const nameInput = popupEditProfile.querySelector('.form__input_type_user-name');
 const occupationInput =  popupEditProfile.querySelector('.form__input_type_user-occupation');
 const submitButtonEditProfile = popupEditProfile.querySelector('.form__submit-btn_place_edit-profile');
 
 const popupAddLocation = document.querySelector('.popup_type_add-location');
-const closeButtonAddLocation = popupAddLocation.querySelector('.button_place_add-location');
+const locationNameInput = popupAddLocation.querySelector('.form__input_type_location-name');
+const locationLinkInput = popupAddLocation.querySelector('.form__input_type_location-link');
+const closeButtonAddCardPopup = popupAddLocation.querySelector('.button_place_add-location');
 const formAddLocation = popupAddLocation.querySelector('.form_type_add-location');
 const submitButtonAddLocation = popupAddLocation.querySelector('.form__submit-btn_place_add-location');
 
@@ -22,54 +26,43 @@ const closeButtonImagePopup = popupViewImage.querySelector('.button_place_image-
 const cards = document.querySelector('.cards');
 const cardTemplate = cards.querySelector('#card-template').content;
 
-
-
-// Обработчики попапа "Редактировать профиль"
-function showPopUpEditProfile() {
-  popupEditProfile.classList.add('popup_opened');
-  popupEditProfile.querySelector('.popup__container').classList.add('popup__container_type_form');
-  nameInput.setAttribute('value', profile.querySelector('.profile__user-name').textContent);
-  occupationInput.setAttribute('value', profile.querySelector('.profile__user-occupation').textContent);
+// Обработчики попапов
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 }
 
-function closePopUpEditProfile() {
-  popupEditProfile.classList.remove('popup_opened');
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+  popup.classList.add('popup_closed');
 }
 
 function handleFormSubmitEditProfile(evt) {
   evt.preventDefault();
-  profile.querySelector('.profile__user-name').textContent = nameInput.value;
-  profile.querySelector('.profile__user-occupation').textContent = occupationInput.value;
-  closePopUpEditProfile();
-}
-
-
-// Обработчики попапа "Новое место"
-function showPopUpAddLocation() {
-  popupAddLocation.querySelector('.popup__container').classList.add('popup__container_type_form');
-  popupAddLocation.classList.add('popup_opened');
-}
-
-function closePopUpAddLocation() {
-  popupAddLocation.classList.remove('popup_opened');
+  userName.textContent = nameInput.value;
+  userOccupation.textContent = occupationInput.value;
+  closePopup(popupEditProfile);
 }
 
 function handleFormSubmitAddLocation(evt) {
   evt.preventDefault();
-  const locationNameInput = popupAddLocation.querySelector('.form__input_type_location-name');
-  const locationLinkInput = popupAddLocation.querySelector('.form__input_type_location-link');
-  handleCard(locationNameInput.value, locationLinkInput.value);
+  addCard(locationNameInput.value, locationLinkInput.value);
   locationNameInput.value = 'Название';
   locationLinkInput.value = 'Ссылка на картинку';
-  closePopUpAddLocation();
+  closePopup(popupAddLocation);
 };
 
+// Обработчики карточек
+function createCard() {
+  initialCards.forEach(function(item) {
+    addCard(item.name, item.link);
+  });
+}
 
-// Обработчики карточки
-function handleCard(name, link) {
-  let card = cardTemplate.querySelector('.card').cloneNode(true);
+function addCard(name, link) {
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
   card.querySelector('.card__location').textContent = name;
   card.querySelector('.card__image').setAttribute('src', link);
+  cards.prepend(card);
   card.querySelector('.button_type_like-button').addEventListener('click', (evt) => {
     evt.target.classList.toggle('button_active');
   });
@@ -78,39 +71,44 @@ function handleCard(name, link) {
   });
   card.querySelector('.card__image').addEventListener('click', function showImagePopup(evt) {
     popupViewImage.classList.toggle('popup_opened');
-    popupViewImage.querySelector('.popup__container').classList.add('popup__container_type_image-popup');
-    const imageLink = card.querySelector('.card__image').getAttribute('src');
-    imageViewImage.setAttribute('src', imageLink);
+    imageViewImage.setAttribute('src', link);
+    imageViewImage.setAttribute('alt', name);
     captionViewImage.textContent = card.querySelector('.card__location').textContent;
   }
   );
-  cards.prepend(card);
+  closeButtonImagePopup.addEventListener('click', () => {
+    closePopup(popupViewImage)
+  });
 }
-
-function closePopUpImagePopup() {
-  popupViewImage.classList.remove('popup_opened');
-}
-
 
 // Добавить начальные карточки
-initialCards.forEach(function(item) {
-  handleCard(item.name, item.link);
-});
-
+createCard();
 
 // Слушатели попапа "Редактировать профиль"
-editButton.addEventListener('click', showPopUpEditProfile);
-formEditProfile.addEventListener('submit', handleFormSubmitEditProfile);
-closeButtonEditProfile.addEventListener('click', closePopUpEditProfile);
+buttonOpenEditProfilePopup.addEventListener('click', () => {
+  openPopup(popupEditProfile);
+  nameInput.setAttribute('value', userName.textContent);
+  occupationInput.setAttribute('value', userOccupation.textContent);
+});
 
+closeButtonEditProfilePopup.addEventListener('click', () => {
+  closePopup(popupEditProfile);
+});
+
+formEditProfile.addEventListener('submit', handleFormSubmitEditProfile);
 
 // Слушатели попапа "Новое место"
-addButton.addEventListener('click', showPopUpAddLocation);
-formAddLocation.addEventListener('submit', handleFormSubmitAddLocation);
-closeButtonAddLocation.addEventListener('click', closePopUpAddLocation);
+buttonOpenAddCardPopup.addEventListener('click', () => {
+  openPopup(popupAddLocation);
+});
 
-// Слушатели попапа "Попап с картинкой"
-closeButtonImagePopup.addEventListener('click', closePopUpImagePopup);
+closeButtonAddCardPopup.addEventListener('click', () => {
+  closePopup(popupAddLocation);
+
+});
+formAddLocation.addEventListener('submit', handleFormSubmitAddLocation);
+
+
 
 
 
